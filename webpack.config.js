9e2ebe10d93a267221ea92e8fad5235fc5701a68
Hung Le <hungcentech@gmail.com>
2019-12-19 09:09:51 +0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  entry: {
+    app: ["./src/App.jsx"]
+  },
+  output: {
+    filename: "app.bundle.js",
+    path: __dirname + "/static"
+  },
+  optimization: {
+    namedChunks: true,
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all"
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  devServer: {
+    port: 8000,
+    proxy: {
+      "/api/*": {
+        target: "http://localhost:3000"
+      }
+    },
+    historyApiFallback: true,
+    contentBase: "static",
+    hot: true
+  },
+  devtool: "source-map",
+  plugins: []
+};
